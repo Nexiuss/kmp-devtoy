e355da49bbf0deaf36efb8c04e6d3297da8bc2fc
Nexiuss <nexiussunny@gmail.com>
2025-12-02 14:10:59 +0800

    [release]1.0.1-alpha

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -81,7 +81,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "com.nexius.devtoy"
-            packageVersion = "1.0.0"
+            packageVersion = "1.0.1-alpha"
         }
     }
 }
