9968cc6632c017de5d543e60f46e687c3c8044d9
Nexiuss <nexiussunny@gmail.com>
2026-02-28 13:39:28 +0800

    「pref」调整包名

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,7 +72,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "com.nexius.devtoy"
+            packageName = "kmp-devtoy"
             packageVersion = "1.0.1"
         }
     }
