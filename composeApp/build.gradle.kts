d6d3d1b705afd2e5a1fbd8420b55418d0b23bfdc
Nexiuss <nexiussunny@gmail.com>
2026-03-13 14:38:40 +0800

    「release」v1.0.2

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -73,7 +73,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "kmp-devtoy"
-            packageVersion = "1.0.1"
+            packageVersion = "1.0.2"
         }
     }
 }
