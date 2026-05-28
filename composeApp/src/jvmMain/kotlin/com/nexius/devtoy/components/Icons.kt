d81b7afe18e691d695e576ac6ae673e04fe9261a
nexiuss <nexiussunny@gmail.com>
2026-05-28 14:53:34 +0800

    [pref]icons细节优化

--- composeApp/src/jvmMain/kotlin/com/nexius/devtoy/components/Icons.kt
+++ composeApp/src/jvmMain/kotlin/com/nexius/devtoy/components/Icons.kt
@@ -556,51 +556,126 @@ object Icons {
 
     val ClearAll: ImageVector
         get() {
-            if (_clearAll != null) return _clearAll!!
-            _clearAll = ImageVector.Builder(
-                name = "clear_all",
-                defaultWidth = 24.dp,
-                defaultHeight = 24.dp,
-                viewportWidth = 24f,
-                viewportHeight = 24f,
-            )
-                .apply {
-                    path(
-                        fill = SolidColor(Color.Black),
-                        fillAlpha = 1f,
-                        stroke = null,
-                        strokeAlpha = 1f,
-                        strokeLineWidth = 1f,
-                        strokeLineCap = StrokeCap.Butt,
-                        strokeLineJoin = StrokeJoin.Bevel,
-                        strokeLineMiter = 1f,
-                        pathFillType = PathFillType.Companion.NonZero,
-                    ) {
-                        moveTo(6f, 19f)
-                        horizontalLineToRelative(12f)
-                        verticalLineToRelative(-2f)
-                        horizontalLineTo(6f)
-                        close()
-                        moveTo(17.64f, 7.22f)
-                        lineToRelative(-1.77f, -1.77f)
-                        lineToRelative(-3.67f, 3.67f)
-                        lineToRelative(-3.67f, -3.67f)
-                        lineToRelative(-1.77f, 1.77f)
-                        lineToRelative(3.67f, 3.67f)
-                        lineToRelative(-3.67f, 3.67f)
-                        lineToRelative(1.77f, 1.77f)
-                        lineToRelative(3.67f, -3.67f)
-                        lineToRelative(3.67f, 3.67f)
-                        lineToRelative(1.77f, -1.77f)
-                        lineToRelative(-3.67f, -3.67f)
-                        close()
+            if (_clear_all != null) {
+                return _clear_all!!
+            }
+            _clear_all =
+                ImageVector.Builder(
+                    name = "clear_all",
+                    defaultWidth = 24.dp,
+                    defaultHeight = 24.dp,
+                    viewportWidth = 24f,
+                    viewportHeight = 24f,
+                )
+                    .apply {
+                        path(
+                            fill = SolidColor(Color.Black),
+                            fillAlpha = 1f,
+                            stroke = null,
+                            strokeAlpha = 1f,
+                            strokeLineWidth = 1f,
+                            strokeLineCap = StrokeCap.Butt,
+                            strokeLineJoin = StrokeJoin.Bevel,
+                            strokeLineMiter = 1f,
+                            pathFillType = PathFillType.Companion.NonZero,
+                        ) {
+                            moveTo(3f, 17f)
+                            verticalLineTo(15f)
+                            horizontalLineTo(17f)
+                            verticalLineToRelative(2f)
+                            horizontalLineTo(3f)
+                            close()
+                            moveTo(5f, 13f)
+                            verticalLineTo(11f)
+                            horizontalLineTo(19f)
+                            verticalLineToRelative(2f)
+                            horizontalLineTo(5f)
+                            close()
+                            moveTo(7f, 9f)
+                            verticalLineTo(7f)
+                            horizontalLineTo(21f)
+                            verticalLineTo(9f)
+                            horizontalLineTo(7f)
+                            close()
+                        }
                     }
-                }
-                .build()
-            return _clearAll!!
+                    .build()
+            return _clear_all!!
         }
 
-    private var _clearAll: ImageVector? = null
+    private var _clear_all: ImageVector? = null
+
+    val SettingsBackupRestore: ImageVector
+        get() {
+            if (_settings_backup_restore != null) {
+                return _settings_backup_restore!!
+            }
+            _settings_backup_restore =
+                ImageVector.Builder(
+                    name = "settings_backup_restore",
+                    defaultWidth = 24.dp,
+                    defaultHeight = 24.dp,
+                    viewportWidth = 24f,
+                    viewportHeight = 24f,
+                )
+                    .apply {
+                        path(
+                            fill = SolidColor(Color.Black),
+                            fillAlpha = 1f,
+                            stroke = null,
+                            strokeAlpha = 1f,
+                            strokeLineWidth = 1f,
+                            strokeLineCap = StrokeCap.Butt,
+                            strokeLineJoin = StrokeJoin.Bevel,
+                            strokeLineMiter = 1f,
+                            pathFillType = PathFillType.Companion.NonZero,
+                        ) {
+                            moveTo(12f, 14f)
+                            quadToRelative(-0.82f, 0f, -1.41f, -0.59f)
+                            reflectiveQuadTo(10f, 12f)
+                            reflectiveQuadToRelative(0.59f, -1.41f)
+                            reflectiveQuadTo(12f, 10f)
+                            reflectiveQuadToRelative(1.41f, 0.59f)
+                            quadTo(14f, 11.18f, 14f, 12f)
+                            reflectiveQuadToRelative(-0.59f, 1.41f)
+                            reflectiveQuadTo(12f, 14f)
+                            close()
+                            moveToRelative(0f, 7f)
+                            quadTo(8.53f, 21f, 5.98f, 18.71f)
+                            quadTo(3.43f, 16.43f, 3.05f, 13f)
+                            horizontalLineTo(5.1f)
+                            quadToRelative(0.35f, 2.6f, 2.31f, 4.3f)
+                            reflectiveQuadTo(12f, 19f)
+                            quadToRelative(2.93f, 0f, 4.96f, -2.04f)
+                            quadTo(19f, 14.93f, 19f, 12f)
+                            quadTo(19f, 9.07f, 16.96f, 7.04f)
+                            reflectiveQuadTo(12f, 5f)
+                            quadTo(10.28f, 5f, 8.78f, 5.8f)
+                            reflectiveQuadTo(6.25f, 8f)
+                            horizontalLineTo(9f)
+                            verticalLineToRelative(2f)
+                            horizontalLineTo(3f)
+                            verticalLineTo(4f)
+                            horizontalLineTo(5f)
+                            verticalLineTo(6.35f)
+                            quadTo(6.28f, 4.75f, 8.11f, 3.88f)
+                            reflectiveQuadTo(12f, 3f)
+                            quadToRelative(1.88f, 0f, 3.51f, 0.71f)
+                            reflectiveQuadToRelative(2.85f, 1.93f)
+                            reflectiveQuadToRelative(1.93f, 2.85f)
+                            reflectiveQuadTo(21f, 12f)
+                            reflectiveQuadToRelative(-0.71f, 3.51f)
+                            reflectiveQuadToRelative(-1.93f, 2.85f)
+                            reflectiveQuadToRelative(-2.85f, 1.93f)
+                            reflectiveQuadTo(12f, 21f)
+                            close()
+                        }
+                    }
+                    .build()
+            return _settings_backup_restore!!
+        }
+
+    private var _settings_backup_restore: ImageVector? = null
 
 
     val Close: ImageVector
